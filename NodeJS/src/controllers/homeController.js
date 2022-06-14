267f37a5a71e693ecd90f33d963f09f192003279
Nguyen Mau Hung <hungnmfx14861@funix.edu.vn>
2022-06-14 13:35:58 +0700

    commit 21 done

--- NodeJS/src/controllers/homeController.js
+++ NodeJS/src/controllers/homeController.js
@@ -1,6 +1,17 @@
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+import { json } from 'body-parser';
+import db from '../models/index'
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
+
 
 let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
